f28bf09caf99e69329dc082d3eddf5ea850970c7
John Carr <john.carr@unrouted.co.uk>
2013-10-27 08:27:58 +0000

    Initial import

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='debhelper',
+    ersion='0.0.0',
+    url="http://yaybu.com/",
+    packages=find_packages(exclude=['ez_setup']),
+    include_package_data=True,
+    zip_safe=False,
+    )
